cb5e8b899ce9c32edc7d86a438230f7760b428a7
syukronrm <syukronrifai@gmail.com>
2020-11-26 22:02:18 +0700

    Parse input on REPL

--- iridium/src/repl/mod.rs
+++ iridium/src/repl/mod.rs
@@ -1,5 +1,8 @@
 use crate::vm::VM;
-use std::io::{self, Read, Write};
+use std::{
+    io::{self, Write},
+    num::ParseIntError,
+};
 
 pub struct REPL {
     command_buffer: Vec<String>,
@@ -35,14 +38,45 @@ impl REPL {
                         println!("{}", command);
                     }
                 }
+                ".program" => {
+                    for instruction in &self.vm.program {
+                        println!("{}", instruction);
+                    }
+                }
                 ".quit" => {
                     println!("Farewell! Have a great day!");
                     std::process::exit(0);
                 }
+                ".registers" => {
+                    println!("Listing registers:");
+                    println!("{:#?}", self.vm.registers);
+                    println!("End of regiter listing")
+                }
                 _ => {
-                    println!("Invalid input");
+                    let result = self.parse_hex(buffer);
+                    match result {
+                        Ok(bytes) => {
+                            self.vm.program.append(&mut bytes.clone());
+                        }
+                        Err(_) => {
+                            println!("Unable to decode hex string. Please enter 4 groups of 2 hex characters.");
+                        }
+                    }
                 }
             }
         }
+    }
+
+    fn parse_hex(&self, i: &str) -> Result<Vec<u8>, ParseIntError> {
+        let split: Vec<&str> = i.split(' ').collect();
+        let mut programs = vec![];
+        for s in split {
+            let byte = s.parse::<u8>();
+            match byte {
+                Ok(b) => programs.push(b),
+                Err(err) => return Err(err),
+            }
+        }
+        Ok(programs)
     }
 }
--- iridium/src/vm.rs
+++ iridium/src/vm.rs
@@ -1,9 +1,9 @@
 use crate::instruction::Opcode;
 
 pub struct VM {
-    registers: [i32; 32],
+    pub registers: [i32; 32],
     pc: usize,
-    program: Vec<u8>,
+    pub program: Vec<u8>,
     remainder: u32,
     equal_flag: bool,
 }
